34f0667727948304e95bab3610f50a97af636cea
tangbzai <tan-jc@qq.com>
2023-12-07 10:25:59 +0800

    feat: 命令行工具判断环境，若支持esm格式时使用mjs代码

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,36 +1,64 @@
-import type { OutputAsset, OutputChunk } from "rollup";
-import resolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
-import commonjs from "@rollup/plugin-commonjs";
-import fs from "fs";
+import { defineConfig } from "rollup"
+import type { OutputAsset, OutputChunk } from "rollup"
+import resolve from "@rollup/plugin-node-resolve"
+import typescript from "@rollup/plugin-typescript"
+import commonjs from "@rollup/plugin-commonjs"
+import fs from "fs"
 
-const pkg = JSON.parse(fs.readFileSync("./package.json").toString());
-const plugins = [resolve(), commonjs(), typescript()];
+const pkg = JSON.parse(fs.readFileSync("./package.json").toString())
+const plugins = [resolve(), commonjs(), typescript()]
 
-const deps = [...Object.keys(pkg.dependencies)];
-const external = (id) => deps.includes(id);
+const deps = [...Object.keys(pkg.dependencies)]
+const external = (id) => deps.includes(id)
 
 function addPrefix(prefix?: string) {
   return {
     name: "addPrefix",
     generateBundle(_, bundleMap: Record<string, OutputAsset | OutputChunk>) {
-      if (!prefix) return;
+      if (!prefix) return
       Object.values(bundleMap).forEach((bundle) => {
         if ("isEntry" in bundle && bundle.isEntry) {
-          bundle.code = prefix + bundle.code;
+          bundle.code = prefix + bundle.code
         }
-      });
+      })
     },
-  };
+  }
+}
+function deleteDirectory(dirPath) {
+  if (fs.existsSync(dirPath)) {
+    fs.readdirSync(dirPath).forEach((file) => {
+      const curPath = `${dirPath}/${file}`
+      if (fs.lstatSync(curPath).isDirectory()) {
+        deleteDirectory(curPath) // 递归删除子目录
+      } else {
+        fs.unlinkSync(curPath) // 删除文件
+      }
+    })
+    fs.rmdirSync(dirPath) // 删除空目录
+  }
 }
 
-export default [
+deleteDirectory("dist")
+fs.mkdirSync("./dist")
+// 拷贝命令行入口文件到产物目录
+fs.copyFileSync("./static/bin.js", "./dist/bin.js")
+export default defineConfig([
   {
     input: "./src/bin.ts",
     output: {
       dir: "dist",
       format: "cjs",
-      entryFileNames: "[name].js",
+      entryFileNames: "[name].cjs",
+    },
+    plugins: [...plugins, addPrefix("#!/usr/bin/env node\n")],
+    external,
+  },
+  {
+    input: "./src/bin.ts",
+    output: {
+      dir: "dist",
+      format: "esm",
+      entryFileNames: "[name].mjs",
     },
     plugins: [...plugins, addPrefix("#!/usr/bin/env node\n")],
     external,
@@ -55,4 +83,4 @@ export default [
     plugins,
     external,
   },
-];
+])
